0f2621424a18ed7b2bc50ab48f46cc10bee319e3
Mugisha-Joshua <o.mugisha2@alustudent.com>
2025-10-27 22:59:19 +0200

    Fixed importing error in widget_test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:slivergrid_demo/main.dart';
 
-import 'package:flutter/lib/main.dart';
+// import 'package:sliver_widget_presentation/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
